a6d15b6d9c98e5c393370ba9e96210d1e7476e9f
Robert G <79031336+AtomicSempai@users.noreply.github.com>
2021-03-11 12:59:34 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Test
+ {
+   public static void main(String args[])
+   { 
+    System.out.println("But I'm only human");
+    System.out.println("And I bleed when I fall down");
+    System.out.println("I'm only human");
+    System.out.println("And I crash and I breakdown");
+   }
+ }
